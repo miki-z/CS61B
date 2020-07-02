69ab4603b51fe77605a2e41682b5da843504991b
Miki <miki@miaomiaojiangs-MacBook-Pro.local>
2020-07-01 21:55:36 -0700

    First commit

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -34,8 +34,13 @@ public class MergeSort {
     /** Returns a queue of queues that each contain one item from items. */
     private static <Item extends Comparable> Queue<Queue<Item>>
             makeSingleItemQueues(Queue<Item> items) {
-        // Your code here!
-        return null;
+        Queue<Queue<Item>> singleItemQ = new Queue<>();
+        for (Item item : items) {
+            Queue<Item> singleQ = new Queue<>();
+            singleQ.enqueue(item);
+            singleItemQ.enqueue(singleQ);
+        }
+        return singleItemQ;
     }
 
     /**
@@ -53,14 +58,36 @@ public class MergeSort {
      */
     private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
             Queue<Item> q1, Queue<Item> q2) {
-        // Your code here!
-        return null;
+        Queue<Item> mergeSorted = new Queue<>();
+        while (!q1.isEmpty() || !q2.isEmpty()) {
+            Item min = getMin(q1, q2);
+            mergeSorted.enqueue(min);
+        }
+        return mergeSorted;
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+        Queue<Queue<Item>> singleQ = makeSingleItemQueues(items);
+        while (singleQ.size() > 1) {
+            Queue<Item> q1 = singleQ.dequeue();
+            Queue<Item> q2 = singleQ.dequeue();
+            singleQ.enqueue(mergeSortedQueues(q1, q2));
+        }
+        return singleQ.dequeue();
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> nums = new Queue<>();
+        nums.enqueue(5);
+        nums.enqueue(1);
+        nums.enqueue(9);
+        nums.enqueue(-3);
+        nums.enqueue(7);
+        nums.enqueue(2);
+        System.out.println("Unsorted queue: " + nums);
+        Queue<Integer> sortedQ = mergeSort(nums);
+        System.out.println("Sorted queue: " + sortedQ);
     }
 }
--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -47,13 +47,45 @@ public class QuickSort {
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
-        // Your code here!
+        for (Item item : unsorted) {
+            if (item.compareTo(pivot) < 0) {
+                less.enqueue(item);
+            } else if (item == pivot) {
+                equal.enqueue(item);
+            } else {
+                greater.enqueue(item);
+            }
+        }
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+        if (items.size() <= 1) {
+            return items;
+        }
+        Queue<Item> less = new Queue<>();
+        Queue<Item> equal = new Queue<>();
+        Queue<Item> greater = new Queue<>();
+        Item pivot = getRandomItem(items);
+        partition(items, pivot, less, equal, greater);
+        Queue<Item> lessItems = quickSort(less);
+        Queue<Item> greaterItems = quickSort(greater);
+        Queue<Item> conect = catenate(lessItems, equal);
+
+        return catenate(conect, greaterItems);
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> nums = new Queue<>();
+        nums.enqueue(5);
+        nums.enqueue(1);
+        nums.enqueue(9);
+        nums.enqueue(-3);
+        nums.enqueue(7);
+        nums.enqueue(2);
+        System.out.println("Unsorted queue: " + nums);
+        Queue<Integer> sortedQ = quickSort(nums);
+        System.out.println("Sorted queue: " + sortedQ);
     }
 }
